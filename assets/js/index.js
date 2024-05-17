c1d1f426b800db8afec76abdd5863dc094f346e4
ksm2024 <katherinesm@hotmail.cl>
2024-05-17 01:20:42 -0400

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,31 @@
 $(document).ready(function () {
-  
+    console.log
+    //  scroll
 
-    
+    $(document).scroll(function (e) {
+        const ubicacionScroll = $("html").scrollTop();
+        console.log("-- ubicacionScroll scroll es --> ", ubicacionScroll);
+        if (ubicacionScroll > 1400) {
+            console.log("Estamos por mayor a 1400");
+            // Cuando pasamos x ubicacionScroll
+            $("footer").removeClass("footerDarck").addClass("footerTransparent")
+        } else {
+            $("footer").removeClass("footerTransparent").addClass("footerDarck")
+            console.log("footer menor a 1400");
+        }
+    });
+
+
+    // button
+    // alert
+    var bttn = $(".enviar")
+    bttn.click(function (e) { alert("Mensaje enviado..."); });
+
+
+
+    // tooltip
+    var myBttns = $(".connectiontooltip")
+    new bootstrap.Tooltip(myBttns[0]);
 
 
 
@@ -10,4 +34,5 @@ $(document).ready(function () {
 
 
 
-  });
+
+});
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Viajes Chile 
+### Es una evaluación que consiste en desarrollo de una landing-page para una agencia de viajes aplicando todos los contendios vistos durante la unidad para lograr una page funcional y similiar a la maqueta propuesta.
+Contenidos aplicados:
+Introducción a HTML.
+● Crear páginas con HTML.
+● Introducción a CSS.
+● Añadiendo CSS al proyecto.
+● Introducción a Bootstrap.
+● Integrando Bootstrap.
+● Usando Bootstrap para personalizar el proyecto.
+● Componentes avanzados de Bootstrap.
+● Introducción a JavaScript.
+● Operaciones con JavaScript.
+● Integrando jQuery.
+● Bootstrap JS.
+● Terminal y manejo de archivos.
+● Introducción a Git.
+● Introducción a Github.
+● Trabajando con Git y Github.
+
+LINK GIT HUB
+
+
+LINK GIT PAGE
