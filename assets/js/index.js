08f3c0cd2d5b125dc5939924642b71b857609f71
ksm2024 <katherinesm@hotmail.cl>
2024-05-17 16:43:28 -0400

    navbar-scrolled

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,20 +2,17 @@ $(document).ready(function () {
     console.log
     //  scroll
 
-    $(document).scroll(function (e) {
-        const ubicacionScroll = $("html").scrollTop();
-        console.log("-- ubicacionScroll scroll es --> ", ubicacionScroll);
-        if (ubicacionScroll > 1400) {
-            console.log("Estamos por mayor a 1400");
-            // Cuando pasamos x ubicacionScroll
-            $("footer").removeClass("footerDarck").addClass("footerTransparent")
-        } else {
-            $("footer").removeClass("footerTransparent").addClass("footerDarck")
-            console.log("footer menor a 1400");
-        }
-    });
+   
+    const navEl = document.querySelector('.navbar-scrolled');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        navEl.classList.add('navbar-scrolled');
+    } else {
+        navEl.classList.remove('navbar-scrolled');
+    }
+});
 
-
+    
     // button
     // alert
     var bttn = $(".enviar")
--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 ### Contenidos aplicados: Introducción a HTML/ Crear páginas con HTML/ Introducción a CSS/Añadiendo CSS al proyecto/Introducción a Bootstrap/ Integrando Bootstrap/ Usando Bootstrap para personalizar el proyecto/Componentes avanzados de Bootstrap/ Introducción a JavaScript/ Operaciones con JavaScript/ Integrando jQuery/ Bootstrap JS/ Terminal y manejo de archivos/Introducción a Git/ Introducción a Github/ Trabajando con Git y Github.
 
 # LINK GIT HUB
-https://github.com/ksm2024/pruebaksm 
+https://github.com/ksm2024/pruebaksm.git
 
 ## LINK GIT PAGE
+https://ksm2024.github.io/pruebaksm/
